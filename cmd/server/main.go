1ac4ecb6bc7d7ba9c745732345e8a628c97f758f
Karwan Shekhe <shekhekarwan@gmail.com>
2025-12-31 16:06:48 +0100

    Add database connection, create 'cloudsave' database, and test server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,14 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"cloudsave/internal/config"
+	"cloudsave/internal/db"
 )
 
 func main() {
+	cfg := config.Load()
+
+	database, err := db.Connect(
+		cfg.DBHost,
+		cfg.DBUser,
+		"CLOUDSAVE#99",
+		cfg.DBName,
+	)
+
+	if err != nil {
+		log.Fatal("Database connection failed: %v", err)
+	}
+
+	defer database.Close()
+
+	fmt.Println("Connecting to database")
+
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
-	fmt.Println("Server running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Server running on :%s\n", cfg.Port)
+	log.Fatal(http.ListenAndServe(":"+cfg.Port, nil))
 }
